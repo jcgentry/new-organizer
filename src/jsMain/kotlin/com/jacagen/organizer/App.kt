84b638a6ad251c7481351fe932db08628a796b38
jcgentry <jcgentry@mac.com>
2024-06-09 22:54:00 -0400

    Clean up outliner a little bit

--- src/jsMain/kotlin/com/jacagen/organizer/App.kt
+++ src/jsMain/kotlin/com/jacagen/organizer/App.kt
@@ -43,9 +43,7 @@ class App : Application() {
 
     override fun start(state: Map<String, Any>) {
         root("kvapp") {
-            div("Start of app")
             vPanel {
-                div("vPanel")
                 outline(rootNode, Node::label, Node::children)
             }
         }
--- src/jsMain/kotlin/com/jacagen/organizer/component/Outline.kt
+++ src/jsMain/kotlin/com/jacagen/organizer/component/Outline.kt
@@ -9,6 +9,7 @@ import io.kvision.form.formPanel
 import io.kvision.form.text.Text
 import io.kvision.form.text.text
 import io.kvision.html.*
+import io.kvision.panel.HPanel
 import io.kvision.panel.hPanel
 import io.kvision.panel.vPanel
 import io.kvision.table.Row
@@ -16,6 +17,9 @@ import io.kvision.utils.auto
 import io.kvision.utils.ch
 import io.kvision.utils.px
 import kotlin.text.Typography.nbsp
+
+
+inline fun <reified T : Any> Container.findChild(): T = getChildren().first { it is T } as T
 
 class Outline<T : Any>(root: T, label: (T) -> String, children: (T) -> List<T>) : Table() {
     var enabledRow: Text? = null
@@ -30,23 +34,20 @@ class Outline<T : Any>(root: T, label: (T) -> String, children: (T) -> List<T>) 
 
     private fun row(indent: Int, node: T, label: (T) -> String) {
         vPanel {
-            var thisText: Text? = null  // yuck
             hPanel {
                 spacer(indent)
                 val panel = FormPanel<T>()
                 panel.apply {
                     val t = resizableText(label(node))
-                    thisText = t
                 }
                 add(panel)
-            }.onClickLaunch {
-                enabledRow?.readonly = true
-                enabledRow = thisText
-                thisText?.readonly = false
             }
+        }.onClickLaunch {
+            findChild<HPanel>().findChild<Text>().readonly = false
         }
     }
 }
+
 
 private fun <T> flattenNode(node: T, children: (T) -> List<T>, level: Int = 0): Sequence<Pair<Int, T>> = sequence {
     yield(Pair(level, node))
