8dbb3160107365c4325ee68d75792b9a3b9062a3
jcgentry <jcgentry@mac.com>
2024-07-04 14:20:39 -0400

    Refactoring display

--- src/jsMain/kotlin/com/jacagen/organizer/App.kt
+++ src/jsMain/kotlin/com/jacagen/organizer/App.kt
@@ -1,8 +1,12 @@
 package com.jacagen.organizer
 
+import com.jacagen.organizer.component.spacer
 import io.kvision.*
+import io.kvision.core.Component
 import io.kvision.core.onClickLaunch
 import io.kvision.html.button
+import io.kvision.panel.HPanel
+import io.kvision.panel.Root
 import io.kvision.panel.root
 import kotlinx.browser.window
 import kotlinx.coroutines.CoroutineScope
@@ -17,21 +21,34 @@ class App : Application() {
         AppScope.launch {
             val controller = Controller.new(console)
             root("kvapp") {
-                try {
-                    add(controller.outline)
-                } catch (e: Exception) {
-                    console.log(e.message ?: "Something went wrong")
-                }
-                button("Show outline").onClickLaunch {
-                    console.log(controller.tree.toString())
-                }
-                button("Add test tree").onClickLaunch { controller.testTree() }
-                button("Show DB").onClickLaunch {
-                    extractOps()
-                }
+                outlineView(controller)
             }
         }
     }
+
+    private fun Root.outlineView(controller: Controller) {
+        try {
+            add(controller.outline)
+        } catch (e: Exception) {
+            console.log(e.message ?: "Something went wrong")
+        }
+        add(buttons(controller))
+    }
+
+    private fun buttons(controller: Controller): Component {
+        return HPanel {
+            button("Show outline").onClickLaunch {
+                console.log(controller.tree.toString())
+            }
+            spacer(.1f)
+            button("Add test tree").onClickLaunch { controller.testTree() }
+            spacer(.1f)
+            button("Show DB").onClickLaunch {
+                extractOps()
+            }
+        }
+    }
+
 
     private suspend fun extractOps() =
         AppScope.launch {
--- src/jsMain/kotlin/com/jacagen/organizer/component/Spacer.kt
+++ src/jsMain/kotlin/com/jacagen/organizer/component/Spacer.kt
@@ -4,13 +4,18 @@ import io.kvision.core.Container
 import io.kvision.html.Div
 import io.kvision.utils.px
 
-class Spacer(size: Int) : Div() {
+class Spacer(size: Float) : Div() {
     init {
-        width = (size * 50).px
+        width = (size * 50.0).toInt().px
     }
 }
 
-fun Container.spacer(size: Int) {
+fun Container.spacer(size: Float) {
     val s = Spacer(size)
+    add(s)
+}
+
+fun Container.spacer(size: Int) {
+    val s = Spacer(size.toFloat())
     add(s)
 }
